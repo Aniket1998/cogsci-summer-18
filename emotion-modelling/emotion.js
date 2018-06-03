4b2a5cd408a2e2b75088f705c04768dfe23935a7
Aniket Das <aniketd@iitk.ac.in>
2018-06-03 17:55:39 +0530

    Complete approach function

--- emotion-modelling/emotion.js
+++ emotion-modelling/emotion.js
@@ -9,21 +9,41 @@ function Person(pos,val,arouse,shape) {
 		if (this.valence > 0) {
 			speed = this.valence/1.5;
 		} else {
-			speed = 3 + 0.2 * this.valence;
+			speed = 3 + 0.25 * this.valence;
 		}
 		return speed;
 	}
 
 	this.perp_perturbations = function(sep,tick) {
-		// Anirudh Anil Ojha Phone Uthao
-		var perp = sep.rotate(90).multiply((13.5 - this.arousal) * Math.sin(tick * this.arousal/10));
+		var perp;
+		if (this.arousal > 0) {
+			if (this.valence < 0) {
+				perp = sep.rotate(90).multiply((this.arousal * 2 + this.valence) * Math.sin(tick * this.arousal/9));
+			} else {
+				perp = sep.rotate(90).multiply((10 - this.valence) * Math.sin(tick * this.arousal/9));
+			}
+		} else {
+			if (this.valence < 0) {
+				perp = sep.rotate(90).multiply(0.2 * (this.arousal  + this.valence) * Math.exp(-tick/100) * Math.sin(tick * this.arousal/60));
+			} else {
+				perp = sep.rotate(90).multiply(2 * (this.arousal - this.valence) * Math.sin(tick * this.arousal/60));
+			}
+		}
 		return perp;
+	}
+
+	this.long_perturbations = function(sep,tick) {
+		var pert;
+		if (this.valence > -4 || this.valence < 4) {
+			pert = sep.rotate((2 * Math.random() - 1) * (this.arousal));
+		}
+		return pert;
 	}
 
 	this.approach = function(person2,tick) {
 		var separation = person2.physique.position.subtract(this.physique.position).normalize();
 		console.log(this.approachspeed());
-		var disp = separation.multiply(this.approachspeed()).add(this.perp_perturbations(separation,tick));
+		var disp = separation.multiply(this.approachspeed()).add(this.perp_perturbations(separation,tick)).add(this.long_perturbations(separation,tick));
 		this.physique.position.x += disp.x;
 		this.physique.position.y += disp.y;
 	}
@@ -37,16 +57,20 @@ function Person(pos,val,arouse,shape) {
 
 var EmotionTable = {
 	joy : {
-		valence : [5,7],
-		arousal : [2,4]
+		valence : [5,7], //6
+		arousal : [2,4] // 3
 	},
 	sadness : {
-		valence : [-9,-6],
-		arousal : [-5,-3],
+		valence : [-9,-6], // -7.5
+		arousal : [-5,-3], // -4
+	},
+	boredom : {
+		valence : [-4,-2], // -3
+		arousal : [-8,-6] // -7
 	},
 	fear : {
-		valence : [-8,-6],
-		arousal : [5,7]
+		valence : [-8,-6], // -7
+		arousal : [5,7] // 6
 	},
 	anger : {
 		valence : [6,8],
