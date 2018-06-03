4a8c4a0dc1d1d08f0fe82990f1a9025e2251d3a1
Aniket Das <aniketd@iitk.ac.in>
2018-06-03 11:42:12 +0530

    Start emotion modelling with valence and arousal

---
+++ emotion-modelling/emotion.js
@@ -0,0 +1,17 @@
+function Person(pos,val,arouse,shape) {
+	this.valence = val;
+	this.arousal = arouse;
+	this.physique = shape;
+	this.physique.position = pos;
+
+	this.approach = function(destination) {
+
+	}
+
+	this.repel = function(destination) {
+
+	}
+}
+var EmotionTable = {
+
+}
