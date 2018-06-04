9363e3d7951bcd8d1d8c1cf3b8f6680124295670
Aniket Das <aniketd@iitk.ac.in>
2018-06-04 14:22:50 +0530

    Add move along path

--- emotion-modelling/emotion_threeparam.js
+++ emotion-modelling/emotion_threeparam.js
@@ -1,41 +1,17 @@
-function Person(shape,pos,focus,energy,eager) {
+function Person(debug,pos,shape) {
 	this.shape = shape;
 	this.shape.position = pos;
-	this.focus = focus;
-	this.energy = energy;
-	this.eager = eager;
+	this.debug = debug;
 
-	this.approach_speed = function(vec,tick) {
-
-	}
-
-	this.perp_perturbations = function(vec,tick) {
-
-	}
-
-	this.long_perturbations = function(vec,tick) {
-
-	}
-
-	this.approach = function(person2,tick) {
-
-	}
-
-	this.repel = function(person2,tick) {
-
-	}
-
-	this.move = function(dest,base,basep,tick) {
-		var vec = dest.subtract(this.shape.position).normalize();
-		var dist = Math.abs(vec.dot(basep));
-		console.log(dist);
-		if (dist < 500) {
-			dist = vec.rotate(40 + 40 * Math.random());
+	this.moveAlongPath = function(path,offset,delta) {
+		if (offset < path.length) {
+			this.shape.position = path.getPointAt(offset);
+			if (this.debug) {
+				console.log(offset);
+			}
+			return (offset + delta * 150);
+		} else {
+			return -1;
 		}
-		this.shape.position.x += dist.x;
-		this.shape.position.y += dist.y;
-	}
-	this.move2 = function(path,offset) {
-
 	}
 }
