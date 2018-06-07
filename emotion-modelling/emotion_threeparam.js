5bb6525e9926051bcfdbcefe6c987e809b1b9446
Aniket Das <aniketd@iitk.ac.in>
2018-06-07 06:25:17 +0530

    Add escape from person

--- emotion-modelling/emotion_threeparam.js
+++ emotion-modelling/emotion_threeparam.js
@@ -79,7 +79,30 @@ function Person(debug,pos,eagerness,arousal,focus,shape) {
 		this.movement.path.strokeColor = 'black';
 	}
 
-	this.retreatFromPerson = function(person2) {
+	this.retreatFromPerson = function(person2,stepsize,numdivs,bound) {
+		var start;
+		if (this.movement.path.length === 0) {
+			start = this.phys.position;
+		} else {
+			start = this.movement.path.getPointAt(this.movement.path.length);
+		}
+		if (this.phys.position.subtract(person2.phys.position).length < bound) {
+			var vx = start.subtract(person2.phys.position);
+			var dist = vx.length;
+			vx = vx.normalize();
+			var vy = vx.rotate(90);
+			var height = 150 * Math.exp(-this.focus/5.0);
+			for (var i = 1; i <= numdivs; i++) {
+				var x = start.add(vx.multiply((stepsize * i)));
+				/*var f = -1;
+				if(i % 2) {
+				f = 1;
+				}*/
+				var y = vy.multiply(height * (2 * Math.random() - 1));
+				this.movement.path.add(x.add(y));
+			}
+			this.movement.path.smooth();
+		}
 	}
 
 	this.followPerson = function(person2) {
