19027cc7386592eff604c31c864ad218b0cf83c0
Aniket1998 <aniketd@iitk.ac.in>
2018-06-20 16:15:44 +0530

    Steering

--- Steering/opensteer.js
+++ Steering/opensteer.js
@@ -11,26 +11,27 @@ function Locomotion(params) {
 	this.position = new Point(params.shape.position.x,params.shape.position.y);
 	this._smoothPos = new Point(params.shape.position.x,params.shape.position.y);
 	this._smoothAcc = new Point(0,0);
+	this._wander_side = 0;
+	this._wander_up = 0;
 
-	this.basisParallel = new Point(0,0);
-	this.basisPerpendicular = new Point(0,0);
+	/*BASIS VECTORS INITIALISED TO RIGHT AND TOP TO MAKE SURE WANDER GIVES SOMETHING RIGHT FROM THE START*/
+	this.basisParallel = new Point(1,0); 
+	this.basisPerpendicular = new Point(0,-1);
 
 	this._adjustForce = function(force,dt) {
 		var maxAdjSpeed = this.maxSpeed * 0.2;
-		if (this.speed > maxAdjSpeed || force.length == 0) {
+		if (this.velocity.length > maxAdjSpeed || force.length == 0) {
 			return force;
 		} else {
-			var range = this.speed/maxAdjSpeed;
+			var range = this.velocity.length/maxAdjSpeed;
 			var cosine = scalar_interpolation(1.0,-1.0,Math.pow(range,20));
 			return limit_max_deviation(force,cosine,this.basisParallel);
 		}
 	}
 
 	this.steer = function(force,dt) {
-
-		//parray marks return of the grid
-
-		var adjForce = this._adjustForce(force,dt);
+		//var adjForce = this._adjustForce(force,dt);
+		var adjForce = force;
 		var acc = adjForce.divide(this.mass);
 
 		if (dt > 0) {
@@ -42,14 +43,10 @@ function Locomotion(params) {
 		this.speed = this.velocity.length;
 		this.position = this.position.add(this.velocity.add(dt));
 
-
-//equivalent to this.vec and vy resp
 		this.basisParallel = this.velocity.normalize();
 		this.basisPerpendicular = this.basisParallel.rotate(90);
 
-		var posSmooth = dt * 0.06;
-
-		//._smoothPos + possmooth * (this.position - _smoothPos)
+		var posSmooth = dt * 0.6;
 		this._smoothPos = blend_vec(posSmooth,this.position,this._smoothPos);
 
 		this.shape.position.x = this._smoothPos.x;
@@ -62,8 +59,64 @@ function Locomotion(params) {
 		Could also use quadratic approximation term 1/2at^2, is it necessary?*/
 		return this.position.add(this.velocity.multiply(time));
 	}
+
+	/* PROBLEM : WITHOUT SOME INITIAL VELOCITY THIS CAN NEVER WORK BECAUSE THE PARALLEL AND PERPENDICULAR
+	VECTORS ARE VELOCITY DEPENDENT AND INITIALISED TO 0*/
+	this.steeringWander = function(dt) {
+		var speed = 12.0 * dt; //TODO : Tune with this
+		this._wander_side = scalar_random_walk(this._wander_side,speed,-1,+1);
+		this._wander_up = scalar_random_walk(this._wander_up,speed,-1,+1);
+		var sideDist = this.basisParallel.multiply(this._wander_side);
+		var perpDist = this.basisPerpendicular.multiply(this._wander_up);
+		return sideDist.add(perpDist);
+	}
+
+	this.steeringSeek = function(target) {
+		var desire = target.subtract(this.position);
+		return desire.subtract(this.velocity);
+	}
+
+	this.steeringFlee = function(target) {
+		var desire = position.subtract(this.target);
+		return desire.subtract(this.velocity);
+	}
+
+	this.predictNearestApproachTime = function(other) {
+		var relative = this.velocity.subtract(other.velocity);
+		var relativeSpeed = relative.length;
+		if (relative.length == 0) {
+			return 0;
+		}
+		var tanget = relative.normalize();
+		var position = this.position.subtract(other.position);
+		var projection = tanget.dot(projection);
+		return projection/relativeSpeed;
+	}
+
+	this.predictNearestApproachPosition = function(other,time) {
+		var mytravel = this.velocity.multiply(time);
+		var othertravel = other.velocity.multiply(time);
+		var myfinal = this.position.add(mytravel);
+		var otherfinal = other.position.add(othertravel);
+		return vector_distance(myfinal,otherfinal);
+	}
 }
 
+function vector_distance(vec1,vec2) {
+	var res = vec1.subtract(vec2);
+	return res.length;
+}
+
+function scalar_random_walk(initial,walkspeed,min,max) {
+	var next = initial + rand_range(-walkspeed,walkspeed);
+	if (next < min) {
+		return min;
+	}
+	if (next > max) {
+		return max;
+	}
+	return next;
+}
 
 //Utility functions for various math ops
 function limit_max_deviation(force,conecos,basis) {
@@ -92,7 +145,6 @@ function vector_interpolation(v1,v2,t) {
 	return v1.add(dif);
 }
 
-//smooth + t * (newvec - smooth)
 function blend_vec(t,newvec,smooth) {
 	return vector_interpolation(smooth,newvec,clip_value(t,0,1))
 }
@@ -113,7 +165,7 @@ function  rand_range(min,max) {
 
 function clip_length(vec,len) {
 	if (vec.length > len) {
-		vec = vec.normalize().multiply(len);
+		return vec.normalize().multiply(len);
 	} else {
 		return vec;
 	}
