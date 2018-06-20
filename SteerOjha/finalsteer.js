f19b1d3af076b6034eef9c5a20c7bf93d1fdb223
AniOjha <aaojha@iitk.ac.in>
2018-06-21 00:54:56 +0530

    Avoid forever

--- SteerOjha/finalsteer.js
+++ SteerOjha/finalsteer.js
@@ -7,8 +7,9 @@ function Locomotion(params) {
 	this.boundingRadius = params.boundingRadius;
 	this.maxForce = params.maxForce;
 	this.maxSpeed = params.maxSpeed;
+	this.shape.position = new Point(params.point.x,params.point.y);
 
-	this.position = new Point(params.shape.position.x,params.shape.position.y);
+	this.position = this.shape.position;
 	this._smoothPos = new Point(params.shape.position.x,params.shape.position.y);
 	this._smoothAcc = new Point(0,0);
 	this._wander_side = 0;
@@ -30,9 +31,11 @@ function Locomotion(params) {
 		}
 	}
 
-	this.steer = function(force,dt) {
-		//var adjForce = this._adjustForce(force,dt);
+	this.steer = function(force,dt, parray) {
+
 		var adjForce = force;
+		//console.log(this.steerToAvoidCollisions(parray).length/this.maxForce + " is ratio");
+		adjForce = adjForce.add(this.steerToAvoidCollisions(parray));
 		var acc = adjForce.divide(this.mass);
 
 		if (dt > 0) {
@@ -95,14 +98,25 @@ function Locomotion(params) {
 
 // (1)
 	this.predictNearestApproachTime = function(other) {
-		var relative = this.velocity.subtract(other.velocity);
+		console.log(this.shape.position+" is this.vel");
+		console.log(other.shape.position+" is obs.vel");
+		
+		var relative = other.velocity.subtract(this.velocity);
 		var relativeSpeed = relative.length;
-		if (relative.length == 0) {
+		
+		if(relativeSpeed == 0){
 			return 0;
 		}
 		var tanget = relative.normalize();
-		var position = this.position.subtract(other.position);
-		var projection = tanget.dot(projection);
+
+		var pos = this.shape.position.subtract(other.shape.position);
+		console.log(tanget + "  " + pos);
+		// if (position.length <= 2 * 15) {
+		// 	return 0;
+		// }
+		var projection = tanget.dot(pos);
+		console.log(projection + " is projection");
+
 		return projection/relativeSpeed;
 	}
 
@@ -110,37 +124,44 @@ function Locomotion(params) {
 	this.predictNearestApproachPosition = function(other,time) {
 		var mytravel = this.velocity.multiply(time);
 		var othertravel = other.velocity.multiply(time);
-		var myfinal = this.position.add(mytravel);
-		var otherfinal = other.position.add(othertravel);
+		var myfinal = this.shape.position.add(mytravel);
+		var otherfinal = other.shape.position.add(othertravel);
 		return vector_distance(myfinal,otherfinal);
 	}
 
 	this.steerToAvoidCollisions = function(parray) {
+		//onsole.log(parray.length + " is array size");
 		if(parray.length) {
 
 			//first see if any immediate threat
 			var minSeparation = 3 * 15;
 			for(var i=0; i<parray.length; i++) {
-				var dist = parray[i].position.subtract(this.position);
+				var dist = this.shape.position.subtract(parray[i].shape.position);
 				if(dist.length < minSeparation) {
-					var proj = dist.dot(this.velocity.normalize());
-					var perp = dist.subtract(proj);
-
-					return perp.multiply(-1);
+					console.log("Oh so close");
+					var vy = this.velocity.rotate(90);
+					// var projn = dist.dot(this.velocity);
+					 console.log(vy.multiply(-4000) + " projn");
+					// if(projn>0) {
+					 	return vy.multiply(-400000).add(this.velocity.multiply(-1000000));
+					// }
+					// else {
+					// 	return vy.multiply(4000);
+					// }
 				}
 			}
 
 			//to avoid obstacles, moving AND stationary
 			var threat = false;
-			var pspace = 6 * 15;
-			var mintimeuntilcollision = 15;			//tune these
+			var pspace = 40 * 15;
+			var mintimeuntilcollision = 40;			//tune these
 
 			var time, mintime = mintimeuntilcollision;
 			var index = 0;
 			for(var i=0; i<parray.length; i++) {
 		
 				time = this.predictNearestApproachTime(parray[i]);
-			
+				console.log(time +" TIME");
 				//if it collides soon enough -
 				if(time >= 0 && time < mintime) {
 
@@ -154,23 +175,24 @@ function Locomotion(params) {
 					}
 				}
 			}
+			console.log(mintime + " is mintime.")
 			if(threat) {
 				var obstacle = parray[index];
 				// Del pos = v * t for now
-				var obs_future = obstacle.position.add(obstacle.velocity.multiply(mintime));
-				var my_future = this.position.add(this.position.multiply(mintime));
+				var obs_future = obstacle.shape.position.add(obstacle.velocity.multiply(mintime));
+				var my_future = this.shape.position.add(this.shape.position.multiply(mintime));
 				var my_unit = this.velocity.normalize();
 				var my_side = my_unit.rotate(90);
 				var obs_unit = obstacle.velocity.normalize();
 				var parallelness = my_unit.dot(obs_unit);
         		var angle = 0.707;
-
+        		//console.log(this.velocity + " pll" + obstacle.velocity);
         		if (parallelness < -angle)
         		{
             		// anti-parallel "head on" paths:
 		            // steer away from future threat position
             		console.log("anti-parallel");
-            		var offset = obs_future.subtract(this.position);
+            		var offset = obs_future.subtract(this.shape.position);
             		var sideDot = offset.dot(my_side);
             		steer = (sideDot > 0) ? -1 : 1;
         		}
@@ -180,7 +202,7 @@ function Locomotion(params) {
             		{
                 		// parallel paths: steer away from threat
                 		console.log("parallel");
-                		offset = obs.position.subtract(this.position);
+                		offset = obstacle.shape.position.subtract(this.shape.position);
             			sideDot = offset.dot(my_side);
             			steer = (sideDot > 0) ? -1 : 1;
             		}
@@ -196,13 +218,16 @@ function Locomotion(params) {
                 		}
             		}
         		}
-
-        		return my_side.multiply(10 * steer);
+        		//offset = this.shaposition.subtract(obstacle.position);
+        		//console.log(my_side.multiply(4000 * steer).add(this.velocity.multiply(-4000)) + " is force");
+        		return my_side.multiply(50000 * steer).add(this.velocity.multiply(-20000));
 
 			} else {
 				//nothing to avoid
-				return new Point();
+				return new Point(0,0);
 			}
+		} else {
+			return new Point(0,0);
 		}
 	}
 
@@ -274,5 +299,84 @@ function clip_length(vec,len) {
 		return vec.normalize().multiply(len);
 	} else {
 		return vec;
+	}
+}
+
+function PersonGrid(width,height) {
+	this.width = width;
+	this.height = height;
+	this.grid = new Array(width);
+	for (var i = this.grid.length - 1; i >= 0; i--) {
+		this.grid[i] = new Array(height);
+		for (var j = this.grid[i].length - 1; j >= 0; j--) {
+			this.grid[i][j] = null;
+		}
+	}
+
+	this.update = function(prev,next,person) {
+		var prevx = Math.floor(prev.x);
+		var prevy = Math.floor(prev.y);
+		var nextx = Math.floor(next.x);
+		var nexty = Math.floor(next.y);
+		if (prevx < 0) {
+			prevx = 0;
+		}
+		if (nextx < 0) {
+			nextx = 0;
+		}
+		if (prevy < 0) {
+			prevy = 0;
+		}
+		if (nexty < 0) {
+		 	nexty = 0;
+		}
+		if (prevx >= this.width) {
+			prevx = this.width - 1;
+		}
+		if (nextx >= this.width) {
+			nextx = this.width - 1;
+		}
+		if (prevy >= this.height) {
+			prevy = this.height - 1;
+		}
+		if (nexty >= this.height) {
+			nexty = this.height - 1;
+		}
+	}
+
+	this.set = function(pos,person) {
+		var x = Math.floor(pos.x);
+		var y = Math.floor(pos.y);
+		if (x >= 0 && y >= 0) {
+			if(x>959) x = 959;
+			if(y>959) y = 959;
+			this.grid[x][y] = person;
+		}
+	}
+
+	this.localSearch = function (startx,endx,starty,endy,except) {
+		startx = Math.floor(startx);
+		endx = Math.floor(endx);
+		starty = Math.floor(starty);
+		endy = Math.floor(endy);
+		if(startx<0) startx = 0;
+		if(starty<0) starty = 0;
+		if(endx<0) endx = 0;
+		if(endy<0) endy = 0;
+
+		if(startx>=960) startx = 959;
+		if(starty>=960) starty = 959;
+		if(endx>=960) endx = 959;
+		if(endy>=960) endy = 959;
+
+		var persons = [];
+		for (var i = startx; i < endx; i++) {
+			for (var j = starty; j < endy; j++) {
+				if (this.grid[i][j] !== null && this.grid[i][j] !== except) {
+					persons.push(this.grid[i][j]);
+				}
+			}
+		}
+		return persons;
 	}
 }
