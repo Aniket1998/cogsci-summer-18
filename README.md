559dec297218c2ff3757f35cbbf8cac675df1d26
Aniket1998 <aniketd@iitk.ac.in>
2018-07-01 14:43:46 +0530

    Massive cleanup of old code

--- README.md
+++
@@ -1,2 +0,0 @@
-# cogsci-summer-18
-Experiments created as part of Summer'18 Cognitive Science Project under Prof. Nisheeth Srivastava
--- cogsciserv/experiments/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Experiment,Response
-# Register your models here.
-admin.site.register(Experiment)
-admin.site.register(Response)
